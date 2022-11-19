fff47b2753b04850d9f8d555c2c44c54c72fc46a
AnrPg <frnephon@mail.com>
2022-11-19 17:57:47 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const redis = require('redis');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379
+});
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+    client.get('visits', (err, visits) => {
+        res.send('Number of visits is ' + visits);
+        client.set('visits', parseInt(visits) + 1);
+    });
+    // res.send('Hi, there!');
+})
+
+app.listen(PORT, HOST, () => {
+    console.log("listening on " + HOST + ":" + PORT);
+});
+
+/*
+'use strict';
+
+const express = require('express');
+const express = require('redis');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
+*/
